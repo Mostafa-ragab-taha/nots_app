d403de55ef3e5b029e46163d0a585c6685e22419
Mostafa <mostafa.ragab.taha.hassan@gmail.com>
2025-08-09 16:10:49 +0300

    add field to bottom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:nots_app/views/widgets/custom_text_field.dart';
 
-class ShowModalBottomSheet extends StatelessWidget
-{
+class ShowModalBottomSheet extends StatelessWidget {
   const ShowModalBottomSheet();
   @override
   Widget build(BuildContext context) {
-    
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         children: [
-          SizedBox(height: 32,),
-          CustomTextField(),],),
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(hintText: "title",maxLines: 1,),
+          CustomTextField(hintText: "content",maxLines: 5,),
+        ],
+      ),
     );
   }
 }
--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:nots_app/constants.dart';
 
 class CustomTextField extends StatelessWidget {
+  CustomTextField({required this.hintText , this.maxLines = 1});
+  final String hintText;
+  final int maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
-      cursorColor: kPrimaryColor,
       
+      cursorColor: kPrimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: "title",
+        hintText: hintText,
+        
         hintStyle: TextStyle(color: kPrimaryColor,),
         border: buildBorder(),
         enabledBorder: buildBorder(),
