39587093f4896f68830c8532a9ba1011da76fd35
Mostafa Ragab <mostafa.ragab.taha.hassan@gmail.com>
2025-08-11 02:52:39 +0300

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:nots_app/constants.dart';
 import 'package:nots_app/views/notes_view.dart';
 
-void main()
+void main()async 
 {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(NotesApp());
 }
 
