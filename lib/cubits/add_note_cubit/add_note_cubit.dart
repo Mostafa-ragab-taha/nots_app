ecfc4c842940c2a89ba7e959500a368860592f3a
Mostafa Ragab <mostafa.ragab.taha.hassan@gmail.com>
2025-08-14 18:59:10 +0300

    Register note type adapter

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nots_app/cubits/add_note_cubit/add_note_state.dart';
+import 'package:nots_app/models/note_model.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState>
+{
+  AddNoteCubit() : super(AddNoteInitial());
+  addNote(NoteModel note)
+  {
+    
+  }
+}
